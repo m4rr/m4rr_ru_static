522c910f342d31f698c4d39f2cfc7fd00d0f4b5f
Marat Saytakov <remarr@gmail.com>
2019-08-21 18:25:49 +0300

    fuck off google maps, welcome apple maps js

---
+++ static/js/amaps.js
@@ -0,0 +1,79 @@
+function mapInitialize() {
+  const tokenID = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlZIQzZQQVVTUTIifQ.eyJpc3MiOiJZNVA3QVg5VU0yIiwiaWF0IjoxNTY2Mzk1NTMzLCJleHAiOjE3MjY3OTA0MDAsIm9yaWdpbiI6Imh0dHA6Ly9sb2NhbGhvc3Q6MTMxMyJ9.f1FqbtMeI4zBcA5eR1KUCOU4wysiWymriLcXOZC3Nu9FJvC6COzwghiAGCREDMJnPrdLW2N1ebVK3fdhEM2xKA";
+
+  mapkit.init({
+      authorizationCallback: function(done) {
+          done(tokenID);
+      }
+  });
+
+
+  $.getJSON("cities.json", function(json) {
+    buildMap(json);
+  });
+
+}
+
+var factory = function(coordinate, options) {
+  var div = document.createElement("div");
+      // name = options.title,           // "John Appleseed"
+      // parts = name.split(' ');        // ["John", "Appleseed"]
+  // div.textContent = parts[0].charAt(0) + parts[1].charAt(0);    // "JA"
+  div.className = "circle-annotation";
+  div.style.backgroundColor = options.color;
+  // div.style.borderColor = options.color2;
+  return div;
+};
+
+function buildMap(markers) {
+  var MarkerAnnotation = mapkit.MarkerAnnotation;
+
+  // mapkit.init({
+  //   authorizationCallback: function (done) {
+  //     var xhr = new XMLHttpRequest();
+  //     xhr.open("GET", "/services/jwt");
+  //     xhr.addEventListener("load", function () {
+  //       done(this.responseText);
+  //     });
+  //     xhr.send();
+  //   }
+  // });
+  var map = new mapkit.Map("map");
+
+  var annotations = markers.map(function (landmark) {
+    var coo = new mapkit.Coordinate(landmark.y, landmark.x);
+    var color = '#'+Math.floor(Math.random()*16777215).toString(16);
+    var color2 = '#'+Math.floor(Math.random()*16777215).toString(16);
+
+    var options = {
+      title: landmark.title_en,
+      color: color,
+      color2: color2
+    };
+
+    var annotation = new mapkit.Annotation(coo, factory, options);
+    // annotation.color = color;
+    annotation.title = landmark.title_en;
+    annotation.subtitle = landmark.title_ru;
+    // annotation.selected = "true";
+    // annotation.glyphText = "✈️";
+
+    return annotation;
+  });
+
+
+
+
+  map.showItems(annotations);
+
+
+  var Cupertino = new mapkit.CoordinateRegion(
+    new mapkit.Coordinate(50, 5),
+    new mapkit.CoordinateSpan(30, 30)
+  );
+
+  map.region = Cupertino;
+}
+
+
+window.onload = mapInitialize;
--- static/js/helpers.js
+++ static/js/helpers.js
@@ -1,44 +1,33 @@
-function isMobile() {
-  var
-    element = document.querySelector('#map'),
-    style = window.getComputedStyle(element),
-    display = style.getPropertyValue('display');
+// function isMobile() {
+//   var
+//     element = document.querySelector('#map'),
+//     style = window.getComputedStyle(element),
+//     display = style.getPropertyValue('display');
 
-  return display === 'none';
-}
+//   return display === 'none';
+// }
 
-function initialize() {
-  if (isMobile()) {
-    return;
-  }
 
-  $.getJSON("cities.json", function(json) {
-    buildMap(json);
-  });
+// function loadScript() {
+//   if (isMobile()) {
+//     return;
+//   }
 
-}
+//   // Preventing the Google Maps libary from downloading an extra font
+//   var head = $('head')[0];
+//   var insertBefore = head.insertBefore;
+//   head.insertBefore = function (newElement, referenceElement) {
+//     // intercept font download
+//     if (newElement.href && newElement.href.indexOf('https://fonts.googleapis.com/css?family=Roboto') === 0) { return; }
+//     // intercept style elements for IEs
+//     if (newElement.tagName.toLowerCase() === 'style' && newElement.styleSheet && newElement.styleSheet.cssText && newElement.styleSheet.cssText.replace('\r\n', '').indexOf('.gm-style') === 0) { return; }
+//     // intercept style elements for other browsers
+//     if (newElement.tagName.toLowerCase() === 'style' && newElement.innerHTML && newElement.innerHTML.replace('\r\n', '').indexOf('.gm-style') === 0) { return; }
 
-function loadScript() {
-  if (isMobile()) {
-    return;
-  }
+//     insertBefore.call(head, newElement, referenceElement);
+//   };
+// }
 
-  // Preventing the Google Maps libary from downloading an extra font
-  var head = $('head')[0];
-  var insertBefore = head.insertBefore;
-  head.insertBefore = function (newElement, referenceElement) {
-    // intercept font download
-    if (newElement.href && newElement.href.indexOf('https://fonts.googleapis.com/css?family=Roboto') === 0) { return; }
-    // intercept style elements for IEs
-    if (newElement.tagName.toLowerCase() === 'style' && newElement.styleSheet && newElement.styleSheet.cssText && newElement.styleSheet.cssText.replace('\r\n', '').indexOf('.gm-style') === 0) { return; }
-    // intercept style elements for other browsers
-    if (newElement.tagName.toLowerCase() === 'style' && newElement.innerHTML && newElement.innerHTML.replace('\r\n', '').indexOf('.gm-style') === 0) { return; }
-
-    insertBefore.call(head, newElement, referenceElement);
-  };
-}
-
-window.onload = loadScript;
 
 $(function() {
   $("a").hover(function() {
