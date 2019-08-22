5a4091ac5f0991e2252a93dd3f4877751b75c84c
Marat Saytakov <remarr@gmail.com>
2019-08-22 03:10:24 +0300

    run-dockers

---
+++ run-dockers.sh
@@ -0,0 +1,27 @@
+cd ~/m4rr_ru_static
+git pull && docker-compose build
+
+cd ~/blog-m4rr-deploy
+git pull && docker-compose build
+
+cd ~
+docker stop nginx-proxy
+
+# to build from scratch:
+# docker run -d -p 80:80 --rm --restart=unless-stopped --name nginx-proxy -e ENABLE_IPV6=true -v /var/run/docker.sock:/tmp/docker.sock jwilder/nginx-proxy
+# https://stackoverflow.com/a/52989931
+
+cd ~/m4rr_ru_static
+docker-compose down
+
+cd ~/blog-m4rr-deploy
+docker-compose down
+docker-compose up -d
+
+cd ~/m4rr_ru_static
+docker-compose up -d
+
+cd ~
+docker start nginx-proxy
+docker network connect m4rrrustatic_default nginx-proxy
+docker network connect blogm4rrdeploy_default nginx-proxy
