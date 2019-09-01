e4f46237a19464cedfc310040576a936f85ed545
Marat Saytakov <remarr@gmail.com>
2019-09-01 14:10:10 +0300

    anchorOffset

--- static/js/amaps.js
+++ static/js/amaps.js
@@ -39,6 +39,7 @@ function buildMap(markers) {
     // annotation.color = color;
     annotation.title = landmark.title_en;
     annotation.subtitle = landmark.title_ru;
+    anchorOffset.anchorOffset.y = 10;
     // annotation.glyphText = "✈️";
 
     return annotation;
