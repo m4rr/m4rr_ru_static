637dc86fab9387cc72b9b6f1de76a92c639a052e
Marat Saytakov <remarr@gmail.com>
2019-10-19 22:20:11 +0700

    forEach

--- static/js/amaps.js
+++ static/js/amaps.js
@@ -24,10 +24,10 @@ function buildMap(markers) {
 
   map.colorScheme = map_color_scheme === "Dark" ? mapkit.Map.ColorSchemes.Dark : mapkit.Map.ColorSchemes.Light;
 
-  var annotations = markers.map(function (landmark) {
-    var coo = new mapkit.Coordinate(landmark.y, landmark.x);
- // var color = '#'+Math.random().toString(16).substr(-6);
-    var hue = Math.random() * 120 - 60; // magentas to yellows (300° to 60°)
+  // var annotations =
+  markers.forEach(function(landmark) {
+
+    // var hue = Math.random() * 120 - 60; // magentas to yellows (300° to 60°)
     // var color = 'hsla(' + (hue < 0 ? hue % 360 + 360 : hue % 360) + ', 100%, 50%, 0.6)';
     var color = map_color_scheme === "Dark" ? 'rgba(255,255,255,0.8)' : 'rgba(160,104,172,0.8)';
 
@@ -38,6 +38,8 @@ function buildMap(markers) {
       }
     };
 
+    var coo = new mapkit.Coordinate(landmark.y, landmark.x);
+
     var annotation = new mapkit.Annotation(coo, factory, options);
     // annotation.color = color;
     annotation.title = landmark.title_en;
@@ -47,10 +49,11 @@ function buildMap(markers) {
     annotation.appearanceAnimation = "zoomIn 0.3s ease-out";
     // annotation.glyphText = "✈️";
 
-    return annotation;
-  });
+    // return annotation;
 
-  map.showItems(annotations);
+    map.addAnnotation(annotation);
+  });
+  // map.showItems(annotations);
 
   var Europe = new mapkit.CoordinateRegion(
     new mapkit.Coordinate(50, 5),
@@ -68,7 +71,7 @@ function buildMap(markers) {
     var domPoint = new DOMPoint(event.pageX, event.pageY);
     var coordinate = map.convertPointOnPageToCoordinate(domPoint);
     map.addAnnotation(new mapkit.MarkerAnnotation(coordinate));
-});
+  });
 
 }
 
