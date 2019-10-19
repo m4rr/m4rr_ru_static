a84eb378067b54038536c4a008aec4e64c00b931
Marat Saytakov <remarr@gmail.com>
2019-10-19 22:54:30 +0700

    a.x > b.x

--- static/js/amaps.js
+++ static/js/amaps.js
@@ -28,7 +28,7 @@ async function buildMap(markers) {
 
   map.colorScheme = map_color_scheme === "Dark" ? mapkit.Map.ColorSchemes.Dark : mapkit.Map.ColorSchemes.Light;
 
-  markers.sort((a,b) => a.y < b.y);
+  markers.sort((a,b) => a.x > b.x);
 
   // var annotations =
   markers.forEach(async function(landmark, index) {
