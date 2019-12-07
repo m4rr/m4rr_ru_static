a3029086b6f35fb8d733b62d02b0f05272bb24ec
Marat Saytakov <remarr@gmail.com>
2019-12-07 11:14:53 +0700

    start using kibana

--- run-deploy.sh
+++ run-deploy.sh
@@ -1,6 +1,8 @@
 git pull
-docker-compose build
-docker stop nginx-proxy
-docker-compose down && docker-compose up -d
-docker network connect m4rrrustatic_default nginx-proxy
-docker start nginx-proxy
+#docker-compose build
+#docker stop nginx-proxy
+#docker-compose down && docker-compose up -d
+#docker network connect m4rrrustatic_default nginx-proxy
+#docker start nginx-proxy
+
+docker-compose up -d --build
